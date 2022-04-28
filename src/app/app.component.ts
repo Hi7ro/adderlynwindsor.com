ae09b8077369840fed398c1e02b91fea26e0e747
xv7ro <xv7ro@xyz.local>
2022-04-28 18:19:58 +0200

    added slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'adderlynwindsor.com';
+export class AppComponent implements OnInit {
+  images = [
+    '../assets/img/code-g8e568109d_1920.jpg',
+    '../assets/img/get-me-out-1605906_1280.jpg',
+    '../assets/img/office.jpg',
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+    setInterval(() => {    
+      this.currentImage++
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 0.8);
+  }, 4000);
+  }
 }
